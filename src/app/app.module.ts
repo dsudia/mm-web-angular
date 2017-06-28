230c063b38d90f5377f7593303c8c996a973d22b
David Sudia <dsudia@fanatics.com>
2017-06-27 21:03:53 -0600

    added toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,13 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http'
 
-import { MdCardModule, MdButtonModule, MdDialogModule, MdInputModule, MdSelectModule } from '@angular/material';
+import { MdCardModule,
+         MdButtonModule,
+         MdDialogModule,
+         MdInputModule,
+         MdSelectModule,
+         MdToolbarModule
+        } from '@angular/material';
 
 // Components
 import { AppComponent } from './app.component';
@@ -14,14 +20,14 @@ import { HowItWorksComponent } from './components/how-it-works/how-it-works.comp
 import { ForSchoolsComponent } from './components/for-schools/for-schools.component';
 import { ForTeachersComponent } from './components/for-teachers/for-teachers.component';
 import { FaqButtonComponent, FaqDialogComponent } from './components/faq/faq.component';
-import { HeaderComponent } from './components/header/header.component';
 import { TermsButtonComponent, TermsDialogComponent } from './components/terms/terms.component';
 import { RegisterFormButtonComponent, RegisterFormDialogComponent } from './components/register-form/register-form.component';
 import { SignInFormButtonComponent, SignInFormDialogComponent } from './components/sign-in-form/sign-in-form.component';
 import { ProfileComponent } from './components/profile/profile.component';
-
+import { ToolbarComponent }
+ from './components/toolbar/toolbar.component'
 // Services
-import { AuthService } from './services/auth/auth.service'
+import { AuthService } from './services/auth/auth.service';
 
 const appRoutes: Routes = [];
 
@@ -34,14 +40,14 @@ const appRoutes: Routes = [];
     ForTeachersComponent,
     FaqButtonComponent,
     FaqDialogComponent,
-    HeaderComponent,
     TermsButtonComponent,
     TermsDialogComponent,
     RegisterFormButtonComponent,
     RegisterFormDialogComponent,
     SignInFormButtonComponent,
     SignInFormDialogComponent,
-    ProfileComponent
+    ProfileComponent,
+    ToolbarComponent
   ],
   entryComponents: [
     FaqDialogComponent,
@@ -57,6 +63,7 @@ const appRoutes: Routes = [];
     MdDialogModule,
     MdInputModule,
     MdSelectModule,
+    MdToolbarModule,
     NoopAnimationsModule,
     ReactiveFormsModule,
     RouterModule.forRoot(appRoutes)
