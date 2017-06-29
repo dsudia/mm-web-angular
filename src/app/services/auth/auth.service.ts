82130711074469281423a916497933e7bbabc1c6
David Sudia <dsudia@fanatics.com>
2017-06-29 10:42:50 -0600

    change path to local server instead of localhost

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -8,7 +8,7 @@ export class AuthService {
   constructor(private http: Http) { }
 
   sendRegistration(email: string, password: string, memberType: number) {
-    return this.http.post('http://localhost:3001/api/v1/auth', {
+    return this.http.post('/api/v1/auth', {
       email,
       password,
       memberType
@@ -24,7 +24,7 @@ export class AuthService {
   }
 
   login(email: string, password: string) {
-    return this.http.get(`http://localhost:3001/api/v1/auth?email=${email}&password=${password}`)
+    return this.http.get(`/api/v1/auth?email=${email}&password=${password}`)
     .toPromise()
     .then(response => {
       const token = response.json().authToken
